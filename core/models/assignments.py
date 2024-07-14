f96f5dcd72a560ea8b4d8ba452a0352248e1eddf
Vishwajeet Patil <113547757+vishwajeetfr@users.noreply.github.com>
2024-07-14 11:55:01 +0530

    to fix getAssignment and gradeAssignment by teacher

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -76,6 +76,7 @@ class Assignment(db.Model):
     def mark_grade(cls, _id, grade, auth_principal: AuthPrincipal):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id, 'This assignment belongs to some other teacher')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
 
         assignment.grade = grade
@@ -89,5 +90,7 @@ class Assignment(db.Model):
         return cls.filter(cls.student_id == student_id).all()
 
     @classmethod
-    def get_assignments_by_teacher(cls):
-        return cls.query.all()
+    def get_assignments_by_teacher(cls, teacher_id):
+        # return cls.query.all()
+        return cls.filter(cls.teacher_id == teacher_id).all()
+
